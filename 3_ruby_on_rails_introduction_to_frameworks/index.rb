edcc1eb33fc1194c8e6cd626cb1c73d519207138
Gabriel MP <contato.gabrielmartinspadoin@gmail.com>
2023-04-18 23:33:58 -0300

    [RAILS] ruby-on-rails-introduction-to-frameworks - feat: read the recommended articles

---
+++ 3_ruby_on_rails_introduction_to_frameworks/index.rb
@@ -0,0 +1,25 @@
+# Introduction
+# Programmers, the best of whom are pretty lazy folk (in a good way), got tired of having to write the same code over and over and over again just to cover the basic tasks that they wanted their applications to perform. So they batched that recycled code together and called it a framework.
+
+# In addition to preventing repetition, frameworks provide great organization. They tend to force you to organize your files and code in a way that keeps it highly modular and really clean. When you start a new app with any framework, you’re given dozens of folders already organized in a hierarchy which makes sense and follows good practice such as Model-View-Controller (MVC) separation principles. It’s not quite “color-by-numbers” for code, but it certainly keeps things ordered.
+
+# There are often several different popular frameworks for a given language. They can have exciting names like Ember, Meteor, Django, Rails, Grok, etc. Wikipedia has a comprehensive comparison of frameworks that should give you an appreciation of the number of them. For Ruby alone, though Rails is the most popular, there are also Sinatra and Padrino and more.
+
+# Lesson Overview
+# This section contains a general overview of topics that you will learn in this lesson.
+
+# What is a framework?
+# What’s the difference between a programming language and a framework?
+# Assignment
+# Get introduced to frameworks by reading this brief article from Dev.to.
+# Glance over this article from RubyGarage or this description from Mozilla’s website to understand some of the thought process that goes into picking a framework.
+# Knowledge Check
+# This section contains questions for you to check your understanding of this lesson on your own. If you’re having trouble answering a question, click it and review the material it links to.
+
+# What problems do frameworks solve?
+# Name some popular front-end and back-end frameworks.
+# Describe the process of choosing a framework.
+# Additional Resources
+# This section contains helpful links to related content. It isn’t required, so consider it supplemental.
+
+# It looks like this lesson doesn’t have any additional resources yet. Help us expand this section by contributing to our curriculum.
